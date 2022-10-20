970dd9cafca8999739e4c40a6e96927ae868c7e9
Andrey <andrey.yarovoy.2013@gmail.com>
2022-10-20 12:26:30 +0300

    152. componentDidUpgrade

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,11 +24,13 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description ? `${char.description.slice(0, 210)}...` : 'Для этого персонажа описание отсутсвует' ,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Для данного персонажа описание отсутсвует' ,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items ? char.comics.items : 'Для данного персонажа комиксы отсутсвуют'
         }
     }
 }
