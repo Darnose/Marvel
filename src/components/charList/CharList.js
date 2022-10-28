6eff0d4190ade0b4f10fc4fa8e40ab9b015a3001
Andrey <andrey.yarovoy.2013@gmail.com>
2022-10-28 13:17:08 +0300

    159. ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,11 +1,13 @@
-import { Component } from 'react/cjs/react.production.min';
+import {Component} from 'react';
 import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 class CharList extends Component {
+
     state = {
         charList: [],
         loading: true,
@@ -14,7 +16,7 @@ class CharList extends Component {
         offset: 210,
         charEnded: false
     }
-
+    
     marvelService = new MarvelService();
 
     componentDidMount() {
@@ -28,14 +30,20 @@ class CharList extends Component {
             .catch(this.onError)
     }
 
-    onCharListLoaded = (newcharList) => {
-        let ended = false
-        if (newcharList.length < 9) {
-            ended = true
+    onCharListLoading = () => {
+        this.setState({
+            newItemLoading: true
+        })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
         }
 
         this.setState(({offset, charList}) => ({
-            charList: [...charList, ...newcharList],
+            charList: [...charList, ...newCharList],
             loading: false,
             newItemLoading: false,
             offset: offset + 9,
@@ -43,21 +51,27 @@ class CharList extends Component {
         }))
     }
 
-    onCharListLoading = () => {
+    onError = () => {
         this.setState({
-            newItemLoading: true
+            error: true,
+            loading: false
         })
     }
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        })
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
     }
 
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -66,8 +80,19 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
@@ -81,14 +106,14 @@ class CharList extends Component {
     }
 
     render() {
-        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
 
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+        
         const items = this.renderItems(charList);
 
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? items : null;
-
 
         return (
             <div className="char__list">
