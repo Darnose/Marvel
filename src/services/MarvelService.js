446e14b42d168cb51bad69a449e8ce24b3c77ad5
Andrey <andrey.yarovoy.2013@gmail.com>
2022-11-30 11:51:24 +0200

    custom hook

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,39 +1,35 @@
-class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=c4222a108c2e0b61e8f3379e2978794b';
-    _baseOffset = 210;
+import {useHttp} from '../hooks/http.hook';
 
-    getResourse = async (url) => {
-        let res = await fetch(url);
+const useMarvelService = () => {
+    const {loading, request, error, clearError} = useHttp();
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status ${res.status}`);
-        }
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=c4222a108c2e0b61e8f3379e2978794b';
+    const _baseOffset = 210;
 
-        return await res.json();
+    const getAllCharacters = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
     }
 
-    getAllCharacters = async (offset = this._baseOffset) => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter);
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
     }
 
-    getCharacter = async (id) => {
-        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0]);
-    }
-
-    _transformCharacter = (char) => {
+    const _transformCharacter = (char) => {
         return {
             id: char.id,
             name: char.name,
-            description: char.description ? `${char.description.slice(0, 210)}...` : 'Для данного персонажа описание отсутсвует' ,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Для данного персонажа описание отсутствует' ,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
-            comics: char.comics.items ? char.comics.items : 'Для данного персонажа комиксы отсутсвуют'
+            comics: char.comics.items ? char.comics.items : 'Для данного персонажа комиксы отсутствуют'
         }
     }
+
+    return {loading, error, getAllCharacters, getCharacter, clearError};
 }
 
-export default MarvelService;
+export default useMarvelService;
